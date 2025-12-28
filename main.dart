994f2f4672bd6d63c7387c0ac5f0e7029875ba07
MineralWater <156356227+zielongs@users.noreply.github.com>
2025-12-28 23:22:31 +0800

    Add new routes and update initial route in main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'tutorlogin.dart';
 import 'tutormenu.dart';
 import 'adminmenu.dart';
 import 'studentregister.dart';
+import 'studentmenu.dart';
+import 'admindashboard.dart';
+import 'studentdashboard.dart';
+import 'profile.dart';
+import 'updateprofile.dart';
 
 void main() {
   runApp(const EduCareApp());
@@ -15,12 +21,22 @@ class EduCareApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/register',
+      title: 'EduCare System',
+      theme: ThemeData(useMaterial3: true),
+
+
+      initialRoute: '/update_profile',
+
       routes: {
         '/': (context) => const TutorLoginScreen(),
         '/tutor_menu': (context) => const TutorMenuScreen(),
         '/admin_menu': (context) => const AdminMenuScreen(),
+        '/admin_dashboard': (context) => const AdminDashboard(),
         '/register': (context) => const StudentRegisterScreen(),
+        '/student_menu': (context) => const StudentMenuScreen(),
+        '/student_dashboard': (context) => const StudentDashboard(),
+        '/profile': (context) => const ProfileScreen(),
+        '/update_profile': (context) => const UpdateProfileScreen(),
       },
     );
   }
